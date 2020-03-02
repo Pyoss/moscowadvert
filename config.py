0c110f91714b365e38b085b7e409b0ce756004ed
Pyoss <yisgit@gmail.com>
2020-03-02 23:19:04 +0300

    Update config.py
    Setting environ for token\id

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-token = "525596163:AAFhvQTlFRWL5yHF12_33_aQwANt9lAMN0g"
-admin_id = 496574763
+token = os.environ['BOT_TOKEN']
+admin_id = os.environ['ADMIN_ID']
 channel_id = '@arendamsk_mo'
 
