fcfb39e9450490bd6b5f44b58382011fb503aa08
Pyoss <yisgit@gmail.com>
2020-07-15 23:26:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-token = os.environ['BOT_TOKEN']
-admin_id = os.environ['ADMIN_ID']
+token = "525596163:AAFhvQTlFRWL5yHF12_33_aQwANt9lAMN0g"
+admin_id = 496574763
 channel_id = '@arendamsk_mo'
 
